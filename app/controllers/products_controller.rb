5d4ae63bea1cc0444fd80ed2a6d6916a0bb3930e
Sean Daly <daly.se91@gmail.com>
2021-02-24 22:16:05 -0600

    Commit 6: Products controller fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :set_product, only: [:show]
 
   # GET /products
   def index
@@ -13,30 +13,7 @@ class ProductsController < ApplicationController
     render json: @product
   end
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -44,8 +21,4 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :price, :img)
-    end
 end

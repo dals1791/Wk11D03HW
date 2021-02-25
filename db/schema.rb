bc2ea3f2344941dda1d98f2f671ba11cb80d2330
Sean Daly <daly.se91@gmail.com>
2021-02-24 21:51:32 -0600

    Commit 3: Products and Reviews associated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_030443) do
+ActiveRecord::Schema.define(version: 2021_02_25_034056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_030443) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "product_id"
   end
 
 end
